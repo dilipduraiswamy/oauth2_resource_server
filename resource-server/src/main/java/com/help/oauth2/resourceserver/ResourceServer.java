7f7ee468346e3f85d340785747f55b92bc1a698c
dilip.duraiswamy <dilip.duraiswamy@BLR-L1718-01692.actcorp.local>
2019-02-05 10:05:37 +0530

    Initial Commit
    Change-Id: I9491e4586d6cc547963bf87c9a6f68c278b2d040
    Change-Id: I0000000001
    Signed-off-by: dilip.duraiswamy <dilip.duraiswamy@BLR-L1718-01692.actcorp.local>

---
+++ resource-server/src/main/java/com/help/oauth2/resourceserver/ResourceServer.java
@@ -0,0 +1,14 @@
+package com.help.oauth2.resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.help.oauth2.resourceserver")
+public class ResourceServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceServer.class);
+    }
+}
